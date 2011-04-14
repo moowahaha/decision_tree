1d985a0bc7bd3fe48c2837b81ec933ee7d891d26
Stephen Hardisty <stephen.hardisty@rea-group.com>
2011-04-14 12:50:38 +1000

    spelled out tests

--- spec/decision_tree_spec.rb
+++ spec/decision_tree_spec.rb
@@ -1,6 +1,6 @@
 describe DecisionTree do
 
-  it "should match our class with attributes" do
+  before do
     candidates = []
 
     [
@@ -12,30 +12,38 @@ describe DecisionTree do
       candidates << Car.new(make: make, model: model, transmission: transmission, price: price)
     end
 
-    tree = DecisionTree.new(
+    @tree = DecisionTree.new(
         candidates: candidates,
         factors: %w{make transmission model}
     )
+  end
 
-    tree.best_for(
+  it "should match the most general only using the make" do
+    @tree.best_for(
         OpenStruct.new(make: 'ford', model: 'whatever', transmission: 'whatever')
     ).price.should == 15000
 
-    tree.best_for(
-        OpenStruct.new(make: 'ford', model: 'focus', transmission: 'whatever')
-    ).price.should == 20000
-
-    tree.best_for(
-        OpenStruct.new(make: 'ford', model: 'focus', transmission: 'automatic')
-    ).price.should == 25000
-
-    tree.best_for(
+    @tree.best_for(
         OpenStruct.new(make: 'posh', model: 'focus', transmission: 'automatic')
     ).price.should == 35000
+  end
 
-    tree.best_for(
+  it "should match the most general only using the make a model" do
+    @tree.best_for(
+        OpenStruct.new(make: 'ford', model: 'focus', transmission: 'whatever')
+    ).price.should == 20000
+  end
+
+  it "should match the most general make, model and transmission" do
+    @tree.best_for(
+        OpenStruct.new(make: 'ford', model: 'focus', transmission: 'automatic')
+    ).price.should == 25000
+  end
+
+  it "should not match if the make is unset" do
+    @tree.best_for(
         OpenStruct.new(make: 'whatever', model: 'focus', transmission: 'automatic')
     ).should be_nil
- end
+  end
 
 end
